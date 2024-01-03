20f15dff5104803d7008cc06b9f2b2afff34ca0c
Tom Dent <tomdent991@hotmail.com>
2024-01-03 19:31:23 +0100

    trying to add style to meter

--- app/components/LevelMeter/LevelMeter.js
+++ app/components/LevelMeter/LevelMeter.js
@@ -1,10 +1,12 @@
+import styles from './LevelMeter.module.css'
+
 export default function LevelMeter( {controlLevel} ) {
 
 
   return (
     <>
       <label htmlFor="volume"></label>
-      <input onChange={(event) => controlLevel(event.target.value)} type="range" id="volume" name="volume" min={0} max={3} step={0.2}>
+      <input className='levelMeter' onChange={(event) => controlLevel(event.target.value)} type="range" id="volume" name="volume" min={0} max={3} step={0.2}>
 
       </input>
     </>
