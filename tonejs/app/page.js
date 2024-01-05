6ef4efd269f0958ab3ae40c9bae7a72f82ab4b74
Tom Dent <tomdent991@hotmail.com>
2024-01-05 11:13:25 +0100

    synth osc working

--- tonejs/app/page.js
+++ tonejs/app/page.js
@@ -1,30 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import * as Tone from 'tone'
+import Image from "next/image";
+import styles from "./page.module.css";
+import dynamic from "next/dynamic";
+
+const Player = dynamic(() => import("../components/Player"), {
+  ssr: false,
+});
+
 
 export default function Home() {
-
-  // const channelOne = new Tone.Player("").toDestination();
-  // const osc = new Tone.Oscillator().toDestination().start();
-  const player = new Tone.Player({
-    url: "/tonejs/public/83 BPM Hip hop chilled.wav",
-    loop: true,
-    loopStart: 0.5,
-    loopEnd: 0.7,
-  }).toDestination();
-
-
-  async function playSynth() {
-    console.log('Before Tone.start()');
-    await Tone.start();
-    console.log('After Tone.start()');
-  }
-
 
   return (
     <main className={styles.main}>
       <p>TEST</p>
-      <button type='submit' onClick={playSynth}>Play</button>
+      <Player />
     </main>
-  )
+  );
 }
