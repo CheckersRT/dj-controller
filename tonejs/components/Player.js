356507c52ae9ae6cda56dd3fa8828c864100af05
Tom Dent <tomdent991@hotmail.com>
2024-01-05 11:52:37 +0100

    plays hiphop wav

--- tonejs/components/Player.js
+++ tonejs/components/Player.js
@@ -4,28 +4,16 @@ import { useEffect } from "react";
 import * as Tone from "tone/build/esm/index";
 
 export default function Player() {
-//   const player = new Tone.Player({
-//       url: "/hiphop.wav",
-//       loop: true,
-//       loopStart: 0.5,
-//       loopEnd: 0.7,
-//     }).toDestination();
 
-  const synth = new Tone.Synth().toDestination();
-  synth.triggerAttackRelease("C4", "8n");
+// const file = URL.createObjectURL()
 
-
+  const player = new Tone.Player({
+      url: "/hiphop.wav",
+    }).toDestination();
 
   async function handlePlay() {
-    console.log("button clicked");
-    await Tone.start();
-    const now = Tone.now();
-      // trigger the attack immediately
-//   synth.triggerAttack("C4", now);
-//   // wait one second before triggering the release
-//   synth.triggerRelease(now + 1);
-
-    console.log("audio is ready");
+    await Tone.start()
+    player.start()
   }
 
   return <button onClick={() => handlePlay()}>Play</button>;
