1463be269f61c79fcc1aaa0d372691f9fa4224f3
Tom Dent <tomdent991@hotmail.com>
2024-01-07 22:46:41 +0100

    crossfader in but needs improving, also mapped eqs

--- components/Mixer/Mixer.js
+++ components/Mixer/Mixer.js
@@ -1,10 +1,17 @@
-
 import KnobEq from "../KnobEq/KnobEq";
 import styles from "./Mixer.module.css";
 import { useRef, useState } from "react";
 import calculateAngle from "@/ultils/calculateAngle";
 
-export default function Mixer({ player1, player2, onCrossFade }) {
+export default function Mixer({
+  player1,
+  player2,
+  crossFade,
+  channel1,
+  channel2,
+  eQsCh1,
+  eQsCh2,
+}) {
   const [mousePosition, setMousePosition] = useState({
     moveX: 0,
     moveY: 0,
@@ -28,7 +35,10 @@ export default function Mixer({ player1, player2, onCrossFade }) {
 
     setAngle(calculateAngle(centerX, centerY, mouseX, mouseY));
 
-    playerOne.current.volume.value = angle / 20;
+    // player1.current.volume.value = angle / 20;
+    eQsCh1.current.low.value = angle / 10;
+    console.log("EQ: ", eQsCh1.current);
+    console.log("EQ value: ", eQsCh1.current.low.value);
   }
 
   function handleMouseMove(event) {
@@ -39,18 +49,65 @@ export default function Mixer({ player1, player2, onCrossFade }) {
     });
   }
 
+  function handleCrossFade(value) {
+    console.log(crossFade.current.fade);
+    console.log(value);
+    crossFade.current.fade.value = value;
+  }
+
+  function handleVolumeFader(event) {
+    console.log(event, channel1.current.volume.value);
+    if (event.target.name === "faderChannel1")
+      player1.current.volume.value = event.target.value;
+    if (event.target.name === "faderChannel2")
+      player2.current.volume.value = event.target.value;
+  }
+
+  const eQArray = ["high", "mid", "low"];
+  const channelArray = ["channel1", "channel2"];
+
   return (
     <div className={styles.container}>
       <p>MIXER</p>
-      <KnobEq
-        $knobAngle={angle}
-        knobImage={knobImageRef}
-        onTurn={handleTurn}
-        setMouseDown={setMouseDown}
-        onMouseMove={handleMouseMove}
-        mouseDown={mouseDown}
-      />
-      <input type="range" min={0} max={1} step={0.1} onChange={(event) => onCrossFade(event.target.value)}></input>
+      {channelArray.map((channel) => {
+        return (
+          <div key={channel}>
+            <div>
+              {eQArray.map((eq) => (
+                <KnobEq
+                  key={eq}
+                  $knobAngle={angle}
+                  knobImage={knobImageRef}
+                  onTurn={handleTurn}
+                  setMouseDown={setMouseDown}
+                  onMouseMove={handleMouseMove}
+                  mouseDown={mouseDown}
+                />
+              ))}
+            </div>
+            <label htmlFor="faderChannel1">Channel 1 fader</label>
+            <input
+              id="faderChannel1"
+              name="faderChannel1"
+              type="range"
+              min={-60}
+              max={0}
+              step={1}
+              onChange={(event) => handleVolumeFader(event)}
+            ></input>
+          </div>
+        );
+      })}
+      <label htmlFor="crossFade">Crossfader</label>
+      <input
+        id="crossFade"
+        name="crossFade"
+        type="range"
+        min={0}
+        max={1}
+        step={0.1}
+        onChange={(event) => handleCrossFade(event.target.value)}
+      ></input>
     </div>
   );
 }
